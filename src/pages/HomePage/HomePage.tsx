e6641365d32f76211c456a0e3a18d770fbc65251
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-10-11 02:15:19 -0300

    test(Homepage): implement random variable to get image or video for the hero on page load

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,30 @@
 import './HomePage.scss';
 
+import { useEffect, useState } from 'react';
+
 import { Hero } from '../../components/Hero/Hero';
 
+import homeHeroVideo from '../../assets/videos/home-hero-video.mp4';
+import homeHeroImage from '../../assets/images/hero-background.jpg';
+
 export const HomePage = () => {
+  const [media, setMedia] = useState({
+    type: 'video',
+    src: homeHeroVideo,
+  });
+
+  useEffect(() => {
+    if (Math.random() < 0.7) {
+      setMedia({
+        type: 'image',
+        src: homeHeroImage,
+      });
+    }
+  }, [media]);
+
   return (
     <>
-      <Hero />
+      <Hero media={media} />
       <h2>Surtidas</h2>
     </>
   );
