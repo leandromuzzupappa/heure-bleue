b75368603a11c0ec431d74212c9a2fe77060f2f6
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-10-11 00:39:13 -0300

    feat(Homepage): create component

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import './HomePage.scss';
+
+import { Hero } from '../../components/Hero/Hero';
+
+export const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <h2>Surtidas</h2>
+    </>
+  );
+};
